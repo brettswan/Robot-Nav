e94f628d0bc6586c20cf9820287cd510ce9b63d1
Haley Kinoshita <kinos103@mail.chapman.edu>
2018-03-05 13:54:14 -0800

    Merge branch 'haley' of https://github.com/brettswan/Robot-Nav
    # Conflicts:
    #	src/Node.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.*;
 
+import static java.lang.Thread.sleep;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -30,14 +32,15 @@ public class Main {
             obstacles = reader.getObstacles();
 
             // Create the start node using the currCoords initial value
-            Node startNode = new Node(currCoords[0],currCoords[1],destCoords[0],destCoords[1],0);
+            Node startNode = new Node(currCoords[0],currCoords[1],destCoords[0],destCoords[1],0, obstacles);
 
             System.out.println(""+startNode.manDistance);
             // Create a new Route with the startNode as the only element
+
             Route path = new Route(startNode);
 
             // Create a queue to store all visited coordinates
-            PriorityQueue<Node> visitedNodes = new PriorityQueue<>();
+            ArrayList<Node> visitedNodes = new ArrayList<Node>();
 
             // Create an array of Routes to store different paths
             ArrayList<Route> routes = new ArrayList<Route>();
@@ -49,22 +52,101 @@ public class Main {
             visitedNodes.add(startNode);
 
             // ACTUAL ALGORITHM
-            while(currCoords != destCoords) {
+            System.out.println(currCoords[0] + "," + currCoords[1]);
+            System.out.println(destCoords[0] + "," + destCoords[1]);
+
+            while(!currCoords.equals(destCoords)) {
                 // Intialize an empy array of Nodes to explore the fringe
-                ArrayList<Node> edgeNodes = new ArrayList<>();
+                //ArrayList<Node> edgeNodes = new ArrayList<>();
 
-                for(int i=0; i<routes.size(); i++) {
+                //for each path in routes, peak on the path (take first val), and insert resunt into edge edgeNodes
 
-                }
+                for(Route r : routes){
 
-                for(Node node : edgeNodes){
-                    int curr_x = node.node_x;
-                    int curr_y = node.node_y;
+                    ArrayList<Node> choices = new ArrayList<>(); // but how are we treating the obsticles
 
-                    Node up = new Node(curr_x,curr_y-1,destCoords[0],destCoords[1],node.cost+1);
-                    Node down = new Node(curr_x,curr_y+1,destCoords[0],destCoords[1],node.cost+1);
-                    Node right = new Node(curr_x+1,curr_y,destCoords[0],destCoords[1],node.cost+1);
-                    Node left = new Node(curr_x-1,curr_y,destCoords[0],destCoords[1],node.cost+1);
+                    Node headNode = r.path.peek();
+
+                    int curr_x = headNode.node_x;
+                    int curr_y = headNode.node_y;
+
+                    // printing variables for terminal
+                    String up_print = "";
+                    String down_print = "";
+                    String right_print = "";
+                    String left_print = "";
+                    ////
+
+                    //up
+                    Node up = new Node(curr_x,curr_y-1,destCoords[0],destCoords[1],headNode.cost+1, obstacles);
+                    if ((!up.isObstacle) && (!visitedNodes.contains(up))){choices.add(up);}
+
+                    //down
+                    Node down = new Node(curr_x,curr_y+1,destCoords[0],destCoords[1],headNode.cost+1,obstacles);
+                    if ((!down.isObstacle) && (!visitedNodes.contains(down))){choices.add(down);}
+
+                    //right
+                    Node right = new Node(curr_x+1,curr_y,destCoords[0],destCoords[1],headNode.cost+1,obstacles);
+                    if ((!right.isObstacle) && (!visitedNodes.contains(right))){choices.add(right);}
+
+                    //left
+                    Node left = new Node(curr_x-1,curr_y,destCoords[0],destCoords[1],headNode.cost+1,obstacles);
+                    if ((!left.isObstacle) && (!visitedNodes.contains(left))){choices.add(left);}
+
+
+                    Node best_node = choices.get(0);
+                    for (Node choice_node : choices){
+                        if (choice_node.totalCost < best_node.totalCost){
+                            best_node = choice_node; // else best node stays the same
+                        }
+                        if (choice_node.totalCost == best_node.totalCost){
+                            // pick one with smallest i value {i,j]
+                            if(choice_node.node_x < best_node.node_x){
+                                best_node = choice_node; // else best node stays the same
+                            }
+                        }
+                    }
+
+                    if(up.isObstacle){ up_print = " + "; }
+                    else{ up_print = Integer.toString(up.totalCost); }
+
+
+                    if(down.isObstacle){ down_print = " + ";}
+                    else{down_print = Integer.toString(down.totalCost);}
+
+
+                    if(right.isObstacle){ right_print = " + ";}
+                    else{right_print = Integer.toString(right.totalCost);}
+
+
+                    if(left.isObstacle){left_print = " + "; }
+                    else{left_print = Integer.toString(left.totalCost);}
+
+                    System.out.println("**************************");
+
+                    System.out.println(headNode.node_x + ", " + headNode.node_y);
+                    System.out.println("\n " + up_print + "  " + "\n" + left_print + " O " + right_print + "\n  " + down_print + "  ");
+                    System.out.println(best_node.totalCost);
+
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    // re assign values
+                    //curr_x = best_node.node_x;
+                    //curr_y = best_node.node_y;
+
+                    //currCoords[0] = best_node.node_x;
+                    //currCoords[1] = best_node.node_y;
+
+                    r.path.push(best_node);
+                    visitedNodes.add(best_node);
+
+
+
                 }
 
             }
--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 import static java.lang.Math.abs;
 import static java.lang.Math.sqrt;
 
@@ -9,8 +13,9 @@ public class Node {
     private int dest_y;
     public int cost;
     public int totalCost;
+    public boolean isObstacle;
 
-    public Node(int x, int y, int dest_x, int dest_y, int cost){
+    public Node(int x, int y, int dest_x, int dest_y, int cost, PriorityQueue<String> obstacles){
         this.cost = cost;
         this.node_x = x;
         this.node_y = y;
@@ -18,5 +23,25 @@ public class Node {
         this.dest_y = dest_y;
         this.manDistance = abs(dest_x - x) + abs(dest_y - y);
         this.totalCost = this.cost + this.manDistance;
+
+        if (obstacles.contains(obs_string_format(node_x,node_y))){
+            this.isObstacle = true;
+        }
+
     }
+
+    public String obs_string_format(int nodex, int nodey){
+        String output = Integer.toString(nodex)+","+Integer.toString(nodey);
+        return output;
+    }
+
+    // outputs which path node should take
+    //public Node decisionFunction(int u, int d, int r, int l){}
+
+    // dont need
+    // (int u, int d, int r, int l)
+    public static int minIndex(ArrayList<Integer> list) {
+        return list.indexOf (Collections.min(list));
+    }
+
 }
